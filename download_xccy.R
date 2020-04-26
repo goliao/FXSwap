a4570c9ae7c002740628e6fe48633fd7c60dfe40
bookworm89 <yugliao@gmail.com>
2020-04-25 20:28:57 -0400

    fixed inverse multiplied and added CNY onshore fwd pt

--- download_xccy.R
+++ download_xccy.R
@@ -7,6 +7,7 @@ require(fst)
 
 
 lookup_tickers <- read_excel('meta/lookup_tickers.xlsx',1) %>% as.data.table()
+lookup_tickers[,.N]
 dtraw <- downloadbbg(lookup_tickers$Ticker,startdt = ymd(20000101),filestr='xccy1y_apr2020_extended.RData',periodstr = 'DAILY' )
 
 dt <- dtraw %>% merge(lookup_tickers,by.x='pk',by.y='Ticker')
@@ -15,8 +16,8 @@ dt <- dtraw %>% merge(lookup_tickers,by.x='pk',by.y='Ticker')
 # dtraw[,.N]
 # dt[,.N]
 dt %>% setnames('Currency','ccy')
-# mxn is quoted with multiplier -1
-dt[ccy!='MXN',value:=-value]
+# mxn is quoted with multiplier -1; do it later
+# dt[ccy=='MXN' & Type=='xccy',value:=-value]
 
 
 saveRDS(dt,file='data/xccy1yextended.rds')
